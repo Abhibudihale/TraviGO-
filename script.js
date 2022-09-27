13858ea5834257e8438e38c3d453d05cc33ad780
Abhi budihale <66393724+Abhibudihale@users.noreply.github.com>
2022-09-27 20:09:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+function myLogin(){
+
+  var myWindow = window.open("https://login2.abhishekbudihal.repl.co/?", "", "toolbar=yes,scrollbars=yes,resizable=yes,top=130px;,left=370px,width=600,height=600");
+ 
+
+};
+
+
+
+
+
+
+
